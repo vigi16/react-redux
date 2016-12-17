ea3bed10e4168f15caa26366e20bf6e5fc347418
vighanesh <vigi16may@gmail.com>
2016-12-17 12:06:39 +0530

    server stuff updated

--- server/index.js
+++ server/index.js
@@ -1,8 +1,26 @@
 import express from 'express';
 import path from 'path';
 
+import webpack from 'webpack';
+
+import webpackMiddleware from 'webpack-dev-middleware';
+import webpackHotMiddleware from 'webpack-hot-middleware';
+import webpackConfig from '../webpack.config.dev';
 
 let app=express();
+
+const compiler = webpack(webpackConfig);
+
+app.use(webpackMiddleware(compiler,{
+
+  hot:true,
+  publicPath: webpackConfig.output.publicPath,
+  noInfo: true
+
+}));
+
+
+app.use(webpackMiddleware(compiler));
 
 app.get('/*',(req,res)=>{
   res.sendFile(path.join(__dirname, './index.html'));
